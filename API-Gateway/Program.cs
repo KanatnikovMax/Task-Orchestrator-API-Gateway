68aa9067898dc3d25647e7b732713c1670586ee5
kanatnikovmaxim@yandex.ru <kanatnikovmaxim@yandex.ru>
2025-11-27 01:13:49 +0300

    add docker

--- API-Gateway/Program.cs
+++ API-Gateway/Program.cs
@@ -1,5 +1,6 @@
 using API_Gateway.IoC;
 using API_Gateway.Realtime.Hubs;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -32,8 +33,10 @@ app.MapHub<TaskProgressHub>("/hubs/task-progress");
 app.MapGet("/{id}", async (string id) =>
 {
     var client = new HttpClient();
-    client.BaseAddress = new Uri("http://localhost:5208");
+    client.BaseAddress = new Uri("http://workers-service:5208");
     await client.GetAsync(id);
 });
+
+app.MapGet("/status", () => Results.Ok(new { Environment.MachineName, Environment.ProcessId, DateTime.Now, DateTime = DateTime.UtcNow, }));
 
 app.Run();
